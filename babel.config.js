a24d10d4013967787767383d3b8fff0e90bd2e21
jaime <jaimerod26@gmail.com>
2025-02-11 21:08:03 -0500

    extract firbase keys to env file

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
     plugins: [
-      // Required for expo-router
       'expo-router/babel',
-      "react-native-reanimated/plugin",
+        [
+          'module:react-native-dotenv',
+          {
+            moduleName: '@env',
+            path: '.env',
+            safe: false,
+            allowUndefined: true,
+          },
+        ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,25 +1,28 @@
 import { initializeApp } from "firebase/app";
-
 import {getReactNativePersistence, initializeAuth} from 'firebase/auth';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {getFirestore, collection} from 'firebase/firestore'
-
-
-// 1. create new project on firebase console
-// 2. enable email and password auth provider in authentication
-// 3. create a web app and copy the firebseConfigs below 
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9-46Wtt2XO0QiwNXibsACa6n42S3TqM0",
-  authDomain: "thorgym-4d10e.firebaseapp.com",
-  projectId: "thorgym-4d10e",
-  storageBucket: "thorgym-4d10e.firebasestorage.app",
-  messagingSenderId: "264722826374",
-  appId: "1:264722826374:web:20ac77ef3e3b52e8aa7d07",
-  measurementId: "G-TRQHR84FLL"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = initializeAuth(app, {
